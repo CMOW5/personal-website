a30f2152703b3430397301ae557a6803669a2845
bit5 <tec.cmos@gmail.com>
2023-04-08 21:53:49 -0500

    now getting the personal data from cloudfront-s3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { PersonalInfoModule } from './personal-info/personal-info.module';
 import { FooterModule } from './footer/footer.module';
 import { AboutModule } from './about/about.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     FontAwesomeModule,
     AboutModule,
     PersonalInfoModule,
